a97229020d5461e83cff06b8ad89cc2f0a3571cd
Angelica Camacho <113385869+Camacho-A@users.noreply.github.com>
2022-11-10 13:44:20 -0500

    Added Food to GitPub

--- server.js
+++ server.js
@@ -5,21 +5,36 @@ const { request, response } = require("express")
 const express = require("express")
 const app = express()
 
-// gets the drinks database (array)
+// gets the drinks & foods database (array)
 const drinks = require("./models/drinks")
 
 app.get("/drinks/", (request, response) => {
 	response.render("drinks_index.ejs", {drinks})
 })
 
+const foods = require("./models/foods")
+
+app.get("/foods/", (request, response) => {
+	response.render("foods_index.ejs", {foods})
+})
+
+// 	Like the HOME PAGE
 app.get("/", (request, response) => {
 	response.send("Welcome to the GitPub App!")
 })
 
+
 app.get("/drinks/:id", (request, response) => {
 	response.render("drinks_show.ejs", {
-		drink: drinks[request.params.id],
+		drink: drinks[request.params.id]
 	
+	})
+})
+
+
+app.get("/foods/:id", (request, response) => {
+	response.render("foods_show.ejs", {
+		food: foods[request.params.id]
 	})
 })
 
